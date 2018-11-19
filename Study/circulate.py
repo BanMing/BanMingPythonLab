8e21c2e74f1368cb62e0b3eeb7959b769a7fc9af
BanMing <banming729@163.com>
2018-11-19 23:03:23 +0800

    first

---
+++ Study/circulate.py
@@ -0,0 +1,10 @@
+names=['Michael','Bob','Tracy']
+for name in names:
+    print(name)
+
+sum=0
+n=99
+while n>0:
+    sum+=n
+    n-=2
+print(sum)
---
+++ Study/condition.py
@@ -0,0 +1,7 @@
+age=input('birth:')
+if int(age)>=18:
+    print('your age is ',age)
+    print('adult')
+else:
+    print('you age is ',age)
+    print('teenager')
